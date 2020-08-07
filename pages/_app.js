d553b63550f657d6cb704593c235e4e6e85c8f52
at1208 <amantiwari12081999@gmail.com>
2020-08-07 10:16:02 +0530

    nextjs boilerplate

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import '../css/style.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'antd/dist/antd.css';
+
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
