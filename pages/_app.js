59b684a6a2ae40ca59d4393c585e19a4aae1b635
at1208 <amantiwari12081999@gmail.com>
2020-08-14 22:19:43 +0530

    added chakra-ui and typescript support

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import '../css/style.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'antd/dist/antd.css';
-
-
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import '../css/style.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'antd/dist/antd.css';
+import { ThemeProvider, CSSReset } from "@chakra-ui/core";
+import { AppProps } from "next/app";
+
+
+function App({ Component, pageProps }: AppProps): React.ReactNode {
+  return (
+    <ThemeProvider>
+      <CSSReset />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
+export default App;
